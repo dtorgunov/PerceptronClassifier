f5f5c666ea6c10bec325aed055db3c86689b5eac
Denis Torgunov <dtorgunov@leafonthewind.net>
2015-09-28 23:46:41 +0100

    Make the 70/30 split a variable
    This should make it easier to make it easier to add this as a variable
    to be passed at a command line.

--- src/Main.hs
+++ src/Main.hs
@@ -21,11 +21,11 @@ takeIndexes (i:is) xs acc = takeIndexes is xs ((xs !! i):acc)
                                        
 
 -- A function to fascilitate verification.
--- Given a list, split it roughly 70/30
-splitList :: (RandomGen g, Eq a) => g -> [a] -> ([a], [a])
-splitList g xs = (training, verification)
+-- Given a list, split it roughly p/(100 - p)
+splitList' :: (RandomGen g, Eq a) => Int -> g -> [a] -> ([a], [a])
+splitList' p g xs = (training, verification)
     where
-      indexes = takeUnique ((length xs - 1) * 70 `div` 100) (randomRs (0, (length xs - 1)) g) []
+      indexes = takeUnique ((length xs - 1) * p `div` 100) (randomRs (0, (length xs - 1)) g) []
       training = takeIndexes indexes xs []
       verification = xs \\ training
 
@@ -34,21 +34,22 @@ usage = do
   progname <- getProgName
   putStrLn ("Usage: " ++ progname ++ " CSV-input-file")
 
-testOn :: String -> IO ()
-testOn filename = do
+testOn :: String -> Int -> IO ()
+testOn filename p = do
   dt <- readCSVData filename
   case dt of
     Left err -> putStrLn err
     Right (classMap, dataSet) -> do
                     let class1 = classMap !! 0
                     let class2 = classMap !! 1
-                    putStrLn ("Assinging classes: " ++ (show class1) ++ ", " ++ (show class2))
+                    putStrLn ("Assigning classes: " ++ (show class1) ++ ", " ++ (show class2))
                     gen <- getStdGen
+                    let splitList = splitList' p
                     let (training, verification) = splitList gen dataSet
                     let network = createNetwork training
                     let results = map (\(x, y) -> x == y) $ map (\(x,y) -> ((runNetwork network) x, y)) verification
                     let errors = length $ filter (==False) results
-                    putStrLn ("After training on 70% of the data set, there were " ++ (show errors) ++ " errors during verification.")
+                    putStrLn ("After training on " ++ (show p) ++ "% of the data set, there were " ++ (show errors) ++ " errors during verification.")
                     putStrLn "Verified on the following values: "
                     mapM_ putStrLn $ map show verification
                     putStrLn "The resulting network is as follows:"
@@ -58,5 +59,5 @@ testOn filename = do
 main :: IO ()
 main = do
   args <- getArgs
-  if (length args) /= 1 then usage else testOn (args !! 0) 
+  if (length args) /= 1 then usage else testOn (args !! 0) 70
   
