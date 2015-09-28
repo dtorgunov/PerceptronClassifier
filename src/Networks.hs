cbe424d685da4aea4fd4902014692da3a7d80e6a
Denis Torgunov <dtorgunov@leafonthewind.net>
2015-09-28 17:06:02 +0100

    Bring comments in accordance with the new code
    Updated the comments, to better reflect the refactored code

--- src/Networks.hs
+++ src/Networks.hs
@@ -17,21 +17,15 @@ sign = signum
 
 -- The following network combinations, for now, assume sign as the activation function
 
--- Complement
--- complementNet :: Network -> Network
--- complementNet n = negate . n
+-- A hyperplane separating plusOne from minusOne, with spacing c
 hyperplane :: Input -> Input -> Double -> Network
 hyperplane plusOne minusOne c = makeNetwork $ Hyperplane plusOne minusOne c
 
 -- Intersection
--- intersectNet :: Network -> Network -> Network
--- n1 `intersectNet` n2 = \xs -> sign $ (n1 xs) + (n2 xs) - 0.5
 intersectNet :: Network -> Network -> Network
 n1 `intersectNet` n2 = makeNetwork $ Intersection (net n1) (net n2)
 
 -- Union
--- unionNet :: Network -> Network -> Network
--- n1 `unionNet` n2 = \xs -> sign $ (n1 xs) + (n2 xs) + 0.5
 unionNet :: Network -> Network -> Network
 n1 `unionNet` n2 = makeNetwork $ Union (net n1) (net n2)
 
@@ -46,6 +40,7 @@ sepFunct u v c = \x -> (x <.> w) - l
       w = zipWith (-) v u
       l = c * (squaredNorm w) - (squaredNorm u) + (u <.> v)
 
+-- Generate a network function based on the network description
 networkFunction :: NetworkDesc -> NetworkFunction
 networkFunction (Hyperplane plusOne minusOne c) = sign . sepFunct minusOne plusOne c
 networkFunction (Union n1 n2) = \xs -> sign $ (n1' xs) + (n2' xs) + 0.5
@@ -57,15 +52,11 @@ networkFunction (Intersection n1 n2) = \xs -> sign $ (n1' xs) + (n2' xs) - 0.5
       n1' = networkFunction n1
       n2' = networkFunction n2
 
+-- Create a network based on the description of its topology
 makeNetwork :: NetworkDesc -> Network
 makeNetwork n = Network (networkFunction n) n
 
+-- Classify a point Input by the Network given
 runNetwork :: Network -> Input -> Classification
 runNetwork n xs = (f n) xs
-
-
--- A basic separating perceptron
--- The "spacing value" of c = 0.5 is hardcoded for this version
--- perc :: Input -> Input -> Network
--- perc plusOne minusOne = sign . sep minusOne plusOne 0.5
 
