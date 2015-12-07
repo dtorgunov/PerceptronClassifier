d40e7dd1086576dc6d59ce2bfa53a3a241a0614e
Denis Torgunov <dtorgunov@leafonthewind.net>
2015-12-07 21:28:51 +0000

    Update signal handling
    Moved from depricated functions to the new way of handling signals using
    the `on` function.

--- src/Gui.hs
+++ src/Gui.hs
@@ -2,6 +2,11 @@ module Main where
 
 import Graphics.UI.Gtk
 import Graphics.UI.Gtk.Builder
+import Control.Monad.Trans(liftIO)
+
+-- Tester signals
+quitMenuActivated :: IO ()
+quitMenuActivated = putStrLn "Quit pressed"
 
 main = do
 
@@ -11,18 +16,28 @@ main = do
   builderAddFromFile builder "toplevel.glade"
 
   -- Get the named items
+  -- The main window
   window <- builderGetObject builder castToWindow "mainWindow"
+
+  -- Combo boxes
   validationMethod <- builderGetObject builder castToComboBox "validationMethod"
   algorithmVersion <- builderGetObject builder castToComboBox "algorithmVersion"
   initialSeparator <- builderGetObject builder castToComboBox "initialSeparator"
 
+  -- Menu items
+  quitMenuItem <- builderGetObject builder castToMenuItem "quitMenuItem"
+
   -- Add ways to exit the application
-  onDestroy window mainQuit
+  --onDestroy window mainQuit
+  window `on` deleteEvent $ liftIO mainQuit >> return False
 
   -- Set the combo boxes to default values
   comboBoxSetActive validationMethod 0
   comboBoxSetActive algorithmVersion 0
   comboBoxSetActive initialSeparator 0
+
+  -- Connect signals
+  quitMenuItem `on` menuItemActivated $ quitMenuActivated
 
   -- Display GUI and run the main application
   widgetShowAll window
