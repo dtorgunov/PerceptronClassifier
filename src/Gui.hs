7bb390427750a980fe0cd0748c7ee1477908097a
Denis Torgunov <dtorgunov@leafonthewind.net>
2015-12-07 20:59:46 +0000

    Update the mock GUI
    Added a few new elements to the mock GUI, without yet adding new functionality.

--- src/Gui.hs
+++ src/Gui.hs
@@ -4,12 +4,26 @@ import Graphics.UI.Gtk
 import Graphics.UI.Gtk.Builder
 
 main = do
+
+  -- Initialise the GUI
   initGUI
---  Just xml <- xmlNew "basicWindw.glade"
---  window <- xmlGetWidget xml castToWindow "applicationWindow1"
   builder <- builderNew
   builderAddFromFile builder "toplevel.glade"
+
+  -- Get the named items
   window <- builderGetObject builder castToWindow "mainWindow"
+  validationMethod <- builderGetObject builder castToComboBox "validationMethod"
+  algorithmVersion <- builderGetObject builder castToComboBox "algorithmVersion"
+  initialSeparator <- builderGetObject builder castToComboBox "initialSeparator"
+
+  -- Add ways to exit the application
   onDestroy window mainQuit
+
+  -- Set the combo boxes to default values
+  comboBoxSetActive validationMethod 0
+  comboBoxSetActive algorithmVersion 0
+  comboBoxSetActive initialSeparator 0
+
+  -- Display GUI and run the main application
   widgetShowAll window
   mainGUI
