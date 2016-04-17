95907114e5d6b12aeb501020786fb084816735ff
Denis Torgunov <dtorgunov@leafonthewind.net>
2016-04-17 21:18:03 +0100

    Add a test for hyperplanes

--- src/Testing.hs
+++ src/Testing.hs
@@ -1,15 +1,25 @@
 {-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE ScopedTypeVariables #-}
 import Test.QuickCheck
 import Test.QuickCheck.Property as P
 import Types
 import Networks
 
-prop_perceptron_equiv =   forAll (vectorOf 5 arbitrary) $ \(a :: [Double]) ->
-                          forAll (vectorOf (length a) arbitrary) $ \(b :: [Double]) ->
-                          forAll (vectorOf (length a) arbitrary) $ \(x :: [Double]) ->
-                          (runNetwork (hyperplane a b 0.5) x) == (applyPerceptron x (generatePerceptron (hyperplane a b 0.5)))
-    -- where
-    --   classifierEquivalence x = (runNetwork (hyperplane a b 0.5) x) == (applyPerceptron x (generatePerceptron (hyperplane a b 0.5)))
+-- Basic property of separating hyperplanes:
 
-main = quickCheck prop_perceptron_equiv
+-- A separating hyperplane built to separate vectors u and v will always classify u and v correctly:
+
+propHyperplaneBasic v = not (null v) ==> forAll (vectorOf (length v) arbitrary) $ \(u :: [Double]) ->
+                        forAll (choose (1,99)) $ \(c :: Int) ->
+                        (runNetwork (hyperplane v u ((fromIntegral c)/100)) v) == 1.0
+                        && (runNetwork (hyperplane v u ((fromIntegral c)/100)) u) == (-1.0)
+
+
+-- Generating random networks to prove their properties
+
+
+-- prop_perceptron_equiv =   forAll (vectorOf 5 arbitrary) $ \(a :: [Double]) ->
+--                           forAll (vectorOf (length a) arbitrary) $ \(b :: [Double]) ->
+--                           forAll (vectorOf (length a) arbitrary) $ \(x :: [Double]) ->
+--                           (runNetwork (hyperplane a b 0.5) x) == (applyPerceptron x (generatePerceptron (hyperplane a b 0.5)))
+
+main = quickCheck propHyperplaneBasic
